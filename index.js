155f05971f2689295f26b63dca766ae99798cfcc
Gabriel Carreon <gabrielcarreon02@gmail.com>
2024-02-26 22:07:00 +0800

    fixed directory

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import {scrap} from "./scrap.js";
-import {scrapObj} from "./scrapObj.js";
+import {scrap} from "./src/scrap.js";
+import {scrapObj} from "./src/scrapObj.js";
 
 if (process.argv.length <= 2){
   throw 'flag missing, try -cpu or -gpu'
