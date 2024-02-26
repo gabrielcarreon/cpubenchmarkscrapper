6ea135c450e427313a8104192cd924e695fccd29
Gabriel Carreon <gabrielcarreon02@gmail.com>
2024-02-26 20:46:02 +0800

    added node script and edited index and readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 npm install
 
 run index.js to scrap cpubenchmark
+
+sometimes the script fails. try again
--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 import puppeteer from "puppeteer"
 import fs from "node:fs"
 
-(async() => {
-  const browser = await puppeteer.launch({ headless: false})
+let tries = 0
+
+const cpuScrap = () => (async() => {
+  const browser = await puppeteer.launch({ headless: true})
   const page = await browser.newPage()
   const regex = /^https:\/\/www\.cpubenchmark\.net\/data\/\?.*$/;
   await page.goto('https://www.cpubenchmark.net/CPU_mega_page.html')
 
   await page.on('response',  async (response)=> {
+    console.log("running scrapper")
     if(regex.test(response.url())){
-      console.log("MATCH")
+      console.log("saving data to out/cpu.json")
       const result = await response.text()
-      await fs.writeFile('output/cpu.json', result, (err) => {
+      if(!fs.existsSync(`./out`)) fs.mkdirSync('./out')
+      await fs.writeFile('out/cpu.json', result, (err) => {
         if(err) throw err
+        tries++
+        if(tries < 3) cpuScrap()
       })
       console.log('Kapag naaalala ko ang mga araw na magkasama tayong dalawaa')
+      await page.close()
+      process.exit()
     }
   })
 
 })()
+
+cpuScrap()
+
